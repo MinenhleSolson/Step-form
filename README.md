2e12758a2a813018b89b235290aa5961ef6cf823
MinenhleSolson <minenhlecele34@gmail.com>
2020-08-18 13:36:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# React.js Static Form
+
+<br>
+
+## Summary
+
+This Is a Static Step-fom which uses React.js and material-ui, in this app I used the react class components to create step by step form. 
+
+User can fill random creditials for testing
+
+## Author
+
+* **Minenhle Solson Cele** - *Full-Stack Software Developer*
---
+++ src/components/Success.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+
+
+export class Success extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+    back = e => {
+        e.preventDefault();
+        this.props.prevStep();   
+    }
+
+
+    render() {
+        return (
+            <MuiThemeProvider>
+               <React.Fragment>
+                <AppBar title="Successfully Registered" />
+                    <h1>Thank you for your Submission</h1>
+                    <p>
+                        We will email you for further instructions
+                    </p>
+               </React.Fragment> 
+            </MuiThemeProvider>
+        )
+    }
+}
+
+
+
+
+export default Success;
